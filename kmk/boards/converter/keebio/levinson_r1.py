535548026e2ecd5e964f8571043c046c73f7cd8e
Bruno Bronosky <bruno@bronosky.com>
2021-04-13 02:02:22 -0500

    Consistent formatting

--- kmk/boards/converter/keebio/levinson_r1.py
+++ kmk/boards/converter/keebio/levinson_r1.py
@@ -5,11 +5,26 @@ from kmk.matrix import DiodeOrientation
 
 
 class KMKKeyboard(_KMKKeyboard):
-    col_pins = (board.A2, board.A3, board.A4, board.A5, board.SCK, board.A0)
-    row_pins = (board.D11, board.D10, board.D9, board.D7)
+    col_pins = (
+        board.A2,
+        board.A3,
+        board.A4,
+        board.A5,
+        board.SCK,
+        board.A0
+    )
+    row_pins = (
+        board.D11,
+        board.D10,
+        board.D9,
+        board.D7
+    )
     diode_orientation = DiodeOrientation.COLUMNS
-    rgb_pixel_pin = board.TX
-    uart_pin = board.SCL
+
     split_type = 'UART'
     split_flip = True
     split_offsets = [6, 6, 6, 6]
+    uart_pin = board.SCL
+    # extra_data_pin = board.SDA
+    rgb_pixel_pin = board.TX
+    # led_pin = board.D7
--- kmk/boards/converter/keebio/quefrency.py
+++ kmk/boards/converter/keebio/quefrency.py
@@ -16,7 +16,14 @@ class KMKKeyboard(_KMKKeyboard):
         board.MOSI,
         board.D12,
     )
-    row_pins = (board.A0, board.D13, board.D11, board.D10, board.D9, board.D7)
+    row_pins = (
+        board.A0,
+        board.D13,
+        board.D11,
+        board.D10,
+        board.D9,
+        board.D7
+    )
     diode_orientation = DiodeOrientation.COLUMNS
     rgb_pixel_pin = board.TX
     uart_pin = board.SCL
